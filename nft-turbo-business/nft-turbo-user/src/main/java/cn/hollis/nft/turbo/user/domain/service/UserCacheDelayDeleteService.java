00301e7f092dad39c7fe0460ecb5667ebda10a27
Hollis <Hollis@noreply.gitcode.com>
2024-07-06 16:55:33 +0800

    rollback

--- nft-turbo-business/nft-turbo-user/src/main/java/cn/hollis/nft/turbo/user/domain/service/UserCacheDelayDeleteService.java
+++ nft-turbo-business/nft-turbo-user/src/main/java/cn/hollis/nft/turbo/user/domain/service/UserCacheDelayDeleteService.java
@@ -2,13 +2,10 @@ package cn.hollis.nft.turbo.user.domain.service;
 
 import cn.hollis.nft.turbo.user.domain.entity.User;
 import com.alicp.jetcache.Cache;
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -20,15 +17,9 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class UserCacheDelayDeleteService {
 
-    private static ThreadFactory userCacheDelayProcessFactory = new ThreadFactoryBuilder()
-            .setNameFormat("user-cache-delay-delete-pool-%d").build();
-
-    private ScheduledExecutorService scheduler = new ScheduledThreadPoolExecutor(10, userCacheDelayProcessFactory);
-
+    @Scheduled(fixedDelay = 2, timeUnit = TimeUnit.SECONDS)
     public void delayedCacheDelete(Cache idUserCache, User user) {
-        scheduler.schedule(() -> {
-            boolean idDeleteResult = idUserCache.remove(user.getId().toString());
-            log.info("idUserCache removed, key = {} , result  = {}", user.getId(), idDeleteResult);
-        }, 2, TimeUnit.SECONDS);
+        boolean idDeleteResult = idUserCache.remove(user.getId().toString());
+        log.info("idUserCache removed, key = {} , result  = {}", user.getId(), idDeleteResult);
     }
 }
--- nft-turbo-common/nft-turbo-base/src/main/java/cn/hollis/nft/turbo/base/request/BaseRequest.java
+++ nft-turbo-common/nft-turbo-base/src/main/java/cn/hollis/nft/turbo/base/request/BaseRequest.java
@@ -7,8 +7,7 @@ import java.io.Serializable;
 
 /**
  * 通用入参
- * 通用入参出参设计: https://thoughts.aliyun.com/workspaces/6655879cf459b7001ba42f1b/docs/6673e7e30f232c0001a8b4a1
- * 文档找不到？没权限怎么办？查看地址：http://nfturbo.wiki/
+ *
  * @author Hollis
  */
 @Setter
