357c7655241f95a0fd2379eb5af827e7c8345c98
Hollis <Hollis@noreply.gitcode.com>
2024-12-31 17:53:54 +0800

    盲盒上线后表结构变更

--- nft-turbo-config/DDL_nfturbo.sql
+++ nft-turbo-config/DDL_nfturbo.sql
@@ -98,6 +98,8 @@ CREATE TABLE `held_collection` (
   `user_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '持有人id',
   `state` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '状态',
   `tx_hash` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '交易hash',
+  `reference_price` decimal(18,6)  NULL COMMENT ' 参考价格',
+  `rarity` varchar(64) NULL COMMENT ' 稀有度',
   `hold_time` datetime DEFAULT NULL COMMENT '藏品持有时间',
   `sync_chain_time` datetime DEFAULT NULL COMMENT '藏品同步时间',
   `delete_time` datetime DEFAULT NULL COMMENT '藏品销毁时间',
@@ -667,4 +669,91 @@ CREATE TABLE `refund_order` (
   KEY `uk_identifier` (`identifier`,`pay_order_id`,`refund_channel`),
   KEY `idx_refund_order` (`refund_order_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+;
+
+/******************************************/
+/*   DatabaseName = nfturbo   */
+/*   TableName = blind_box   */
+/******************************************/
+CREATE TABLE `blind_box` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID（自增主键）',
+  `gmt_create` datetime DEFAULT NULL COMMENT '创建时间',
+  `gmt_modified` datetime DEFAULT NULL COMMENT '最后更新时间',
+  `name` varchar(512) DEFAULT NULL COMMENT '盲盒名称',
+  `cover` varchar(512) DEFAULT NULL COMMENT '盲盒封面',
+  `detail` text COMMENT '详情',
+  `identifier` varchar(128) DEFAULT NULL COMMENT '幂等号',
+  `state` varchar(128) DEFAULT NULL COMMENT '状态',
+  `quantity` bigint DEFAULT NULL COMMENT '盲盒数量',
+  `price` decimal(18,6) DEFAULT NULL COMMENT '价格',
+  `saleable_inventory` bigint DEFAULT NULL COMMENT '可销售库存',
+  `occupied_inventory` bigint DEFAULT NULL COMMENT '已占用库存',
+  `create_time` datetime DEFAULT NULL COMMENT '盲盒创建时间',
+  `sale_time` datetime DEFAULT NULL COMMENT '盲盒发售时间',
+  `allocate_rule` varchar(512) DEFAULT NULL COMMENT '盲盒分配规则',
+  `sync_chain_time` datetime DEFAULT NULL COMMENT '上链时间',
+  `creator_id` varchar(128) DEFAULT NULL COMMENT '创建者',
+  `collection_configs` text COMMENT '藏品配置',
+  `deleted` int DEFAULT NULL COMMENT '是否逻辑删除，0为未删除，非0为已删除',
+  `lock_version` int DEFAULT NULL COMMENT '乐观锁版本号',
+  PRIMARY KEY (`id`),
+  KEY `idx_state_name` (`state`,`name`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb3 COMMENT='盲盒表'
+;
+
+/******************************************/
+/*   DatabaseName = nfturbo   */
+/*   TableName = blind_box_inventory_stream   */
+/******************************************/
+CREATE TABLE `blind_box_inventory_stream` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID（自增主键）',
+  `gmt_create` datetime NOT NULL COMMENT '创建时间',
+  `gmt_modified` datetime NOT NULL COMMENT '最后更新时间',
+  `blind_box_id` bigint DEFAULT NULL COMMENT '盲盒id',
+  `changed_quantity` bigint DEFAULT NULL COMMENT '本次变更的数量',
+  `price` decimal(18,6) DEFAULT NULL COMMENT '价格',
+  `quantity` bigint DEFAULT NULL COMMENT '藏品数量',
+  `state` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '状态',
+  `saleable_inventory` bigint DEFAULT NULL COMMENT '可售库存',
+  `occupied_inventory` bigint DEFAULT NULL COMMENT '已占库存',
+  `stream_type` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '流水类型',
+  `identifier` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '幂等号',
+  `deleted` int DEFAULT NULL COMMENT '是否逻辑删除，0为未删除，非0为已删除',
+  `lock_version` int DEFAULT NULL COMMENT '乐观锁版本号',
+  `extend_info` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '扩展信息',
+  PRIMARY KEY (`id`),
+  KEY `idx_cid_ident_type` (`identifier`,`stream_type`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=561 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci AVG_ROW_LENGTH=16384 ROW_FORMAT=DYNAMIC COMMENT='盲盒表库存流水'
+;
+
+/******************************************/
+/*   DatabaseName = nfturbo   */
+/*   TableName = blind_box_item   */
+/******************************************/
+CREATE TABLE `blind_box_item` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID（自增主键）',
+  `gmt_create` datetime DEFAULT NULL COMMENT '创建时间',
+  `gmt_modified` datetime DEFAULT NULL COMMENT '最后更新时间',
+  `blind_box_id` bigint DEFAULT NULL COMMENT '盲盒id',
+  `name` varchar(512) DEFAULT NULL COMMENT '盲盒名称',
+  `cover` varchar(512) DEFAULT NULL COMMENT '盲盒封面',
+  `collection_name` varchar(512) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '藏品名称',
+  `collection_cover` varchar(512) DEFAULT NULL COMMENT '藏品封面',
+  `collection_detail` text COMMENT '藏品详情',
+  `collection_serial_no` varchar(128) DEFAULT NULL COMMENT '持有藏品的序列号',
+  `state` varchar(128) DEFAULT NULL COMMENT '状态',
+  `user_id` varchar(128) DEFAULT NULL COMMENT '持有人id',
+  `purchase_price` decimal(18,6) DEFAULT NULL COMMENT '购入价格',
+  `order_id` varchar(128) DEFAULT NULL COMMENT '订单号',
+  `deleted` int DEFAULT NULL COMMENT '是否逻辑删除，0为未删除，非0为已删除',
+  `lock_version` int DEFAULT NULL COMMENT '乐观锁版本号',
+  `rarity` varchar(32) DEFAULT NULL COMMENT '稀有度',
+  `reference_price` decimal(18,6) DEFAULT NULL COMMENT '市场参考价',
+  `opened_time` datetime DEFAULT NULL COMMENT ' 开盒时间',
+  `assign_time` datetime DEFAULT NULL COMMENT ' 分配时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_state_box_id` (`blind_box_id`,`state`),
+  KEY `idx_user` (`order_id`),
+  KEY `idx_order` (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4011 DEFAULT CHARSET=utf8mb3 COMMENT='盲盒条目表'
 ;
