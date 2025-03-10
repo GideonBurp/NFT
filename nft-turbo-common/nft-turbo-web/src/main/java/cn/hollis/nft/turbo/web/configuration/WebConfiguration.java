aab523dd4918dda36c12b44aaced03711fe40481
javabagu <>
2025-03-10 18:51:28 +0800

    普通交易加入TokenFilter校验

--- nft-turbo-common/nft-turbo-web/src/main/java/cn/hollis/nft/turbo/web/configuration/WebConfiguration.java
+++ nft-turbo-common/nft-turbo-web/src/main/java/cn/hollis/nft/turbo/web/configuration/WebConfiguration.java
@@ -1,8 +1,5 @@
 package cn.hollis.nft.turbo.web.configuration;
 
-import cn.dev33.satoken.filter.SaServletFilter;
-import cn.dev33.satoken.same.SaSameUtil;
-import cn.dev33.satoken.util.SaResult;
 import cn.hollis.nft.turbo.web.filter.TokenFilter;
 import cn.hollis.nft.turbo.web.handler.GlobalWebExceptionHandler;
 import org.redisson.api.RedissonClient;
@@ -11,7 +8,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -38,7 +34,7 @@ public class WebConfiguration implements WebMvcConfigurer {
         FilterRegistrationBean<TokenFilter> registrationBean = new FilterRegistrationBean<>();
 
         registrationBean.setFilter(new TokenFilter(redissonClient));
-        registrationBean.addUrlPatterns("/trade/buy","/trade/newBuy");
+        registrationBean.addUrlPatterns("/trade/buy","/trade/newBuy","/trade/normalBuy");
         registrationBean.setOrder(10);
 
         return registrationBean;
