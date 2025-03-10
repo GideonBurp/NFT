b2451af78ccc5ffb6f97dca22dc0393ca06bd08c
javabagu <>
2025-03-10 18:40:30 +0800

    增加TCC模块，实现TCC基本功能

---
+++ nft-turbo-common/nft-turbo-tcc/src/main/java/cn/hollis/nft/turbo/tcc/entity/TransActionLogState.java
@@ -0,0 +1,13 @@
+package cn.hollis.nft.turbo.tcc.entity;
+
+/**
+ * @author Hollis
+ */
+public enum TransActionLogState {
+
+    TRY,
+
+    CONFIRM,
+
+    CANCEL
+}
---
+++ nft-turbo-common/nft-turbo-tcc/src/main/java/cn/hollis/nft/turbo/tcc/service/TransactionLogService.java
@@ -0,0 +1,122 @@
+package cn.hollis.nft.turbo.tcc.service;
+
+import cn.hollis.nft.turbo.tcc.entity.TransActionLogState;
+import cn.hollis.nft.turbo.tcc.entity.TransCancelSuccessType;
+import cn.hollis.nft.turbo.tcc.entity.TransactionLog;
+import cn.hollis.nft.turbo.tcc.mapper.TransactionLogMapper;
+import cn.hollis.nft.turbo.tcc.request.TccRequest;
+import cn.hollis.nft.turbo.tcc.response.TransactionCancelResponse;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+/**
+ * 盲盒服务
+ *
+ * @author Hollis
+ */
+public class TransactionLogService extends ServiceImpl<TransactionLogMapper, TransactionLog> {
+
+    /**
+     * TCC事务的Try
+     *
+     * @param tccRequest
+     * @return
+     */
+    public Boolean tryTransaction(TccRequest tccRequest) {
+        TransactionLog existTransactionLog = getExistTransLog(tccRequest);
+        if (existTransactionLog == null) {
+            TransactionLog transactionLog = new TransactionLog(tccRequest, TransActionLogState.TRY);
+            return this.save(transactionLog);
+        }
+
+        //幂等
+        if (existTransactionLog.getState() == TransActionLogState.TRY || existTransactionLog.getState() == TransActionLogState.CONFIRM) {
+            return true;
+        }
+
+        //如果已经Cancel，直接返回成功，避免事务悬挂
+        if (existTransactionLog.getState() == TransActionLogState.CANCEL) {
+            return true;
+        }
+
+        throw new UnsupportedOperationException("unsupport state :" + existTransactionLog.getState());
+    }
+
+    /**
+     * TCC事务的Confirm
+     *
+     * @param tccRequest
+     * @return
+     */
+    public Boolean confirmTransaction(TccRequest tccRequest) {
+        TransactionLog existTransactionLog = getExistTransLog(tccRequest);
+        if (existTransactionLog == null) {
+            throw new UnsupportedOperationException("transacton can not confirm");
+        }
+
+        if (existTransactionLog.getState() == TransActionLogState.TRY) {
+            existTransactionLog.setState(TransActionLogState.CONFIRM);
+            return this.updateById(existTransactionLog);
+        }
+
+        //幂等
+        if (existTransactionLog.getState() == TransActionLogState.CONFIRM) {
+            return true;
+        }
+
+        throw new UnsupportedOperationException("transacton can not confirm :" + existTransactionLog.getState());
+    }
+
+    /**
+     * TCC事务的Cancel
+     *
+     * @param tccRequest
+     * @return
+     */
+    public TransactionCancelResponse cancelTransaction(TccRequest tccRequest) {
+        TransactionLog existTransactionLog = getExistTransLog(tccRequest);
+        //如果还没有Try，则直接记录一条状态为Cancel的数据，避免发生空回滚，并解决悬挂问题
+        if (existTransactionLog == null) {
+            TransactionLog transactionLog = new TransactionLog(tccRequest, TransActionLogState.CANCEL, TransCancelSuccessType.EMPTY_CANCEL);
+            if (this.save(transactionLog)) {
+                return new TransactionCancelResponse(true, TransCancelSuccessType.EMPTY_CANCEL);
+            }
+            return new TransactionCancelResponse(false, "EMPTY_CANCEL_FAILED", "EMPTY_CANCEL_FAILED");
+        }
+
+        if (existTransactionLog.getState() == TransActionLogState.TRY) {
+            existTransactionLog.setState(TransActionLogState.CANCEL);
+            existTransactionLog.setCancelType(TransCancelSuccessType.CANCEL_AFTER_TRY_SUCCESS);
+            if (this.updateById(existTransactionLog)) {
+                return new TransactionCancelResponse(true, TransCancelSuccessType.CANCEL_AFTER_TRY_SUCCESS);
+            }
+            return new TransactionCancelResponse(false, "CANCEL_FAILED", "CANCEL_FAILED");
+        }
+
+        if (existTransactionLog.getState() == TransActionLogState.CONFIRM) {
+            existTransactionLog.setState(TransActionLogState.CANCEL);
+            existTransactionLog.setCancelType(TransCancelSuccessType.CANCEL_AFTER_CONFIRM_SUCCESS);
+            if (this.updateById(existTransactionLog)) {
+                return new TransactionCancelResponse(true, TransCancelSuccessType.CANCEL_AFTER_CONFIRM_SUCCESS);
+            }
+            return new TransactionCancelResponse(false, "CANCEL_FAILED", "CANCEL_FAILED");
+        }
+
+        //幂等
+        if (existTransactionLog.getState() == TransActionLogState.CANCEL) {
+            return new TransactionCancelResponse(true, TransCancelSuccessType.DUPLICATED_CANCEL);
+        }
+
+        return new TransactionCancelResponse(false, "CANCEL_FAILED", "CANCEL_FAILED");
+    }
+
+    private TransactionLog getExistTransLog(TccRequest request) {
+        QueryWrapper<TransactionLog> queryWrapper = new QueryWrapper<TransactionLog>();
+        queryWrapper.eq("transaction_id", request.getTransactionId());
+        queryWrapper.eq("business_scene", request.getBusinessScene());
+        queryWrapper.eq("business_module", request.getBusinessModule());
+
+        return this.getOne(queryWrapper);
+    }
+
+}
